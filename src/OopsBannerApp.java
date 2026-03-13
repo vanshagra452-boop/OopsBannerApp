b4367c9f9dd4075c1d140d8cdc3cc73544831df6
Vansh Agrawal <vanshagra452@gmail.com>
2026-03-13 10:33:31 +0530

    UC4: Banner using array and loop

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -2,13 +2,19 @@ public class OopsBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(String.join("", " ***** ", "  ", " ***** ", "  ", " ***** ", "  ", " ***** ", "  ", " ***** "));
-        System.out.println(String.join("", "*     *", "  ", "*     *", "  ", "*     *", "  ", "*     *", "  ", "*      "));
-        System.out.println(String.join("", "*     *", "  ", "*     *", "  ", "*     *", "  ", "*     *", "  ", "*      "));
-        System.out.println(String.join("", "*     *", "  ", "*     *", "  ", " ***** ", "  ", " ***** ", "  ", " ***** "));
-        System.out.println(String.join("", "*     *", "  ", "*     *", "  ", "*      ", "  ", "*      ", "  ", "      *"));
-        System.out.println(String.join("", "*     *", "  ", "*     *", "  ", "*      ", "  ", "*      ", "  ", "      *"));
-        System.out.println(String.join("", " ***** ", "  ", " ***** ", "  ", "*      ", "  ", "*      ", "  ", " ***** "));
+        String[] banner = {
+            String.join("", " ***** ", "  ", " ***** ", "  ", " ***** ", "  ", " ***** ", "  ", " ***** "),
+            String.join("", "*     *", "  ", "*     *", "  ", "*     *", "  ", "*     *", "  ", "*      "),
+            String.join("", "*     *", "  ", "*     *", "  ", "*     *", "  ", "*     *", "  ", "*      "),
+            String.join("", "*     *", "  ", "*     *", "  ", " ***** ", "  ", " ***** ", "  ", " ***** "),
+            String.join("", "*     *", "  ", "*     *", "  ", "*      ", "  ", "*      ", "  ", "      *"),
+            String.join("", "*     *", "  ", "*     *", "  ", "*      ", "  ", "*      ", "  ", "      *"),
+            String.join("", " ***** ", "  ", " ***** ", "  ", "*      ", "  ", "*      ", "  ", " ***** ")
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
 
     }
 }
